e4978afc6d14230643ec8b48113f45bb45a59b70
xctrln <xiesansi@gmail.com>
2018-06-21 10:02:24 +0800

    my bubble sort

---
+++ sorting/my_bubble_sort.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+import "github.com/0xAX/go-algorithms"
+
+func main(){
+	arr := util.RandArray(10)
+	alen = len(arr)
+
+	for(i := 0; i < alen; i++){
+		for(j := 0; j < alen - 1 - i; j++){
+			if(arr[j] > arr[j+1]){
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
