a1f8a401a82975f835f5fcc65bed4d59019d7979
xctrln <xiesansi@gmail.com>
2018-06-21 10:36:40 +0800

    add flag, indicating no change throught last j epoch

--- sorting/my_bubble_sort.go
+++ sorting/my_bubble_sort.go
@@ -7,12 +7,17 @@ import "github.com/0xAX/go-algorithms"
 func main(){
 	arr := utils.RandArray(10)
 	alen := len(arr)
+	flag := 0
 
 	for i := 0; i < alen; i++ {
 		for j := 0; j < alen - 1 - i; j++ {
 			if(arr[j] > arr[j+1]){
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				flag = 1
 			}
+		}
+		if flag == 0{
+			break
 		}
 	}
 
