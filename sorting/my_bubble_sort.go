d7cdc52e03e6c70a3925aa700a21000ddd128ea2
xctrln <xiesansi@gmail.com>
2018-06-21 10:05:11 +0800

    remove "(" and ")" in for loop

--- sorting/my_bubble_sort.go
+++ sorting/my_bubble_sort.go
@@ -8,11 +8,13 @@ func main(){
 	arr := util.RandArray(10)
 	alen = len(arr)
 
-	for(i := 0; i < alen; i++){
-		for(j := 0; j < alen - 1 - i; j++){
+	for i := 0; i < alen; i++ {
+		for j := 0; j < alen - 1 - i; j++ {
 			if(arr[j] > arr[j+1]){
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 		}
 	}
+
+	fmt.Println(arr)
 }
