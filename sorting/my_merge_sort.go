825c7d47e3b98e94b9ea8fb40ff23686a6f36171
xctrln <xiesansi@gmail.com>
2018-06-24 13:46:44 +0800

    sync

--- sorting/my_merge_sort.go
+++ sorting/my_merge_sort.go
@@ -2,40 +2,41 @@ package main
 
 import "fmt"
 
+import "github.com/0xAX/go-algorithms"
 
 func merge(arr1 []int, arr2 []int) []int  {
-	i := 0
-	j := 0
-	n := 0
-	nlen := len(arr1) + len(arr2)
-	narr := make([]int, nlen) 
-	for ; j + i < nlen;  {
-		if arr1[i] > arr2[j] {
-			narr[n] = arr2[j]
-			j++
-		}else{
-			narr[n] = arr1[i]
-			i++
-		}
-		n++
-	}
-	return narr
+        i := 0
+        j := 0
+        n := 0
+        nlen := len(arr1) + len(arr2)
+        var narr []int
+        for ; j + i < nlen;  {
+                if arr1[i] > arr2[j] {
+                        narr[n] = arr2[j]
+                        j++
+                }else{
+                        narr[n] = arr1[i]
+                        i++
+                }
+                n++
+        }
+        return narr
 }
 
 func MergeSort(arr []int) []int {
-	if len(arr) == 1{
-		return arr
-	}else{
-		arr1 := arr[0:len(arr)/2]
-		arr2 := arr[len(arr)/2 + 1 : len(arr) - 1]
-		left := MergeSort(arr1)
-		right := MergeSort(arr2)
-		return merge(left, right)
-	}
+        if len(arr) == 1{
+                return arr
+        }else{
+                arr1 := arr[0:len(arr)/2]
+                arr2 := arr[len(arr)/2 + 1 : len(arr) - 1]
+                left := MergeSort(arr1)
+                right := MergeSort(arr2)
+                return merge(left, right)
+        }
 }
 
 func main(){
-	arr := utils.RandArray(10)
-	narr := MergeSort(arr)
-	fmt.Println(narr)
+        arr := utils.RandArray(10)
+        narr := MergeSort(arr)
+        fmt.Println(narr)
 }
